cedf2a2f99df066ff7da9c8c615f14a8fcb2ca03
Eugene Angelo <dev.eugene.paje@gmail.com>
2020-10-18 08:03:32 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikiService } from './wiki.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wiki-search';
+  pages = [];
+
+  constructor(private wiki: WikiService) { }
+
+  onTerm(term: string) {
+    this.wiki.search(term).subscribe((response: any) => this.pages = response.query.search);
+  }
 }
